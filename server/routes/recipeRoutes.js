9dc749073750d54b289579f7a2f9ea69c1fe24d5
venkatrao99 <mvenkychowdary9999@gmail.com>
2024-02-06 01:50:38 +0530

    Your commit message here

---
+++ server/routes/recipeRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const recipeController = require('../controllers/recipeController');
+
+/**
+ * App Routes 
+*/
+router.get('/', recipeController.homepage);
+router.get('/recipe/:id', recipeController.exploreRecipe );
+//router.post('/recipe', recipeController.exploreRecipeonpost);
+router.get('/categories', recipeController.exploreCategories);
+router.get('/categories/:id', recipeController.exploreCategoriesById);
+router.post('/search', recipeController.searchRecipe);
+router.get('/explore-latest', recipeController.exploreLatest);
+router.get('/explore-random', recipeController.exploreRandom);
+router.get('/submit-recipe', recipeController.submitRecipe);
+router.post('/submit-recipe', recipeController.submitRecipeOnPost);
+//router.get('/cart/:id', recipeController.cart);
+//router.get('/cart',recipeController.cart);
+
+
+ 
+module.exports = router;
